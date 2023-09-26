844781402243dbd0e0e697145c34a854e224b45d
Luís Eduardo Luz <98917362+azzdud@users.noreply.github.com>
2023-09-26 20:33:07 -0300

    Atividade 08

--- mod_produto/ProdutoDAO.py
+++ mod_produto/ProdutoDAO.py
@@ -5,7 +5,13 @@ from mod_produto.Produto import Produto
 import db
 from mod_produto.ProdutoModel import ProdutoDB
 
-router = APIRouter()
+from fastapi import Depends
+import security
+
+# dependências de forma global
+router = APIRouter(
+    dependencies=[Depends(security.verify_token), Depends(security.verify_key)]
+)
 # Criar as rotas/endpoints: GET, POST, PUT, DELETE
 
 
